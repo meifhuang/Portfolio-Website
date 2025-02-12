b6eb3b1a13f4070e05ec26b02cdf8c6eabe5ff14
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2025-02-12 15:45:21 -0500

    remove previous htmlcssjs

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const nav = document.querySelector("#hamburger-nav")
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    nav.classList.toggle("open")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
